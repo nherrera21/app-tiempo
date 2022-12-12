95ac478721145e0a54ac1df06b742baa57fb29d1
nherrera21 <noehf21@gmail.com>
2022-12-12 18:30:47 +0100

    edicion

---
+++ database/migrations/2022_12_11_221210_update_weather_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateWeatherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('weather', function (Blueprint $table) {
+            $table->string('zipcode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather');
+    }
+}
